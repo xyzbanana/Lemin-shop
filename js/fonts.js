8f28cc7550c880f4db71f35cb0f0ddfd97b161d2
bannana <115848045+xyzbanana@users.noreply.github.com>
2023-07-30 01:40:38 +0700

    Add files via upload

---
+++ js/fonts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var options = {
+      strings: ['Hello, I am Thecuong!', 'a Hair Stylist', 'a Image Editor', 'a Beautiful Hair Photo', 'a Love Money', 'Cường đẹp zai'],
+      typeSpeed: 130,
+      backSpeed: 60,
+      loop: true
+    };
+    var typed = new Typed("#lemin-text", options);
+  });

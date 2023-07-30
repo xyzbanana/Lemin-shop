ff7e4b02d1b1200f7bffcb0997f0519d4064b368
bannana <115848045+xyzbanana@users.noreply.github.com>
2023-07-30 23:17:25 +0700

    Delete js directory

--- js/fonts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var options = {
-      strings: ['Hello, I am Thecuong!', 'a Hair Stylist', 'a Image Editor', 'a Beautiful Hair Photo', 'a Love Money', 'Cường đẹp zai'],
-      typeSpeed: 130,
-      backSpeed: 60,
-      loop: true
-    };
-    var typed = new Typed("#lemin-text", options);
-  });
